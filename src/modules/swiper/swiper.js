5e845e27684cec8d508ce68fdc46f0ebff5e77af
alheym <alheym.mw@gmail.com>
2023-01-31 08:14:43 +0300

    swiper

--- src/modules/swiper/swiper.js
+++ src/modules/swiper/swiper.js
@@ -1,9 +1,15 @@
 import './_swiper.scss';
 
 var swiper = new Swiper(".swiperText", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    speed: 800,
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  speed: 800,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  }
+});
